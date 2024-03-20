e216fbd438d5bc617444efc906fd01c22d3cc2d5
DEBUG-DONOT <877845066@qq.com>
2024-03-20 17:37:21 +0800

    把移动放到view矩阵里面了

--- Camera.cpp
+++ Camera.cpp
@@ -2,6 +2,7 @@
 #include<glm/gtc/matrix_transform.hpp>
 
 Camera::Camera(glm::vec3 pos, glm::vec3 lookat, glm::vec3 up)
+	:pos(pos),lookat(lookat),up(up)
 {
 	this->view = glm::lookAt(pos, lookat, up);
 	this->model = glm::mat4(1.0f);
@@ -23,17 +24,24 @@ void Camera::ProcessInput(GLFWwindow* window,float deltaTime)
 {
 	float cameraSpeed = speed * deltaTime;
 	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-		this->model = glm::translate(this->model, this->cameraFront * -cameraSpeed);
+		//this->model = glm::translate(this->model, this->cameraFront * -cameraSpeed);
+		this->pos += this->cameraFront * cameraSpeed;
 	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-		this->model = glm::translate(this->model, this->cameraFront * cameraSpeed);
+		//this->model = glm::translate(this->model, this->cameraFront * cameraSpeed);
+		this->pos -= this->cameraFront * cameraSpeed;
 	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-		this->model = glm::translate(this->model, this->cameraRight * cameraSpeed);
+		//this->model = glm::translate(this->model, this->cameraRight * cameraSpeed);
+		this->pos -= this->cameraRight * cameraSpeed;
 	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-		this->model = glm::translate(this->model, this->cameraRight * -cameraSpeed);
+		//this->model = glm::translate(this->model, this->cameraRight * -cameraSpeed);
+		this->pos += this->cameraRight * cameraSpeed;
 	if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
-		this->model = glm::translate(this->model, this->cameraUp * -cameraSpeed);
+		//this->model = glm::translate(this->model, this->cameraUp * -cameraSpeed);
+		this->pos += this->cameraUp * cameraSpeed;
 	if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
-		this->model = glm::translate(this->model, this->cameraUp * cameraSpeed);
+		//this->model = glm::translate(this->model, this->cameraUp * cameraSpeed);
+		this->pos -= this->cameraUp * cameraSpeed;
+	this->view= glm::lookAt(pos, pos+cameraFront, up);
 }
 
 
